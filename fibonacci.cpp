63c6225973cf2d5791f4d973da436a228bee7e9a
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-06-02 11:47:50 -0300

    fibonacci comments added

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Recursive function of Fibonacci
 int fib(int n) {
+  // If n == 1 || n == 0, then its fib is itself.
   if (n <= 1) return n;
 
+  // Else, fib of n is the sum of its two antecessors, n-1 and n-2
   return fib(n - 1) + fib(n - 2);
 }
 
