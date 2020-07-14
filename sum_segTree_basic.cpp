24a0b6c34bb9722a4edb9dbe2aed702d17bea4b8
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-14 15:11:52 -0300

    segTree test program added on main function

--- sum_segTree_basic.cpp
+++ sum_segTree_basic.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define MAXN 100100
 // Initializing data structures, the segtree must have 4 times more space than original array
-int n, segtree[4 * MAXN], array[MAXN];
+int segtree[4 * MAXN], arr[MAXN];
 
 // SegmentTree build function
 // Must run in main with initial values, like: (v == 1, tl == 0, tr == n - 1)
@@ -29,7 +29,8 @@ int sum(int v, int tl, int tr, int l, int r) {
   if (l == tl && r == tr) return segtree[v];
   // Otherwise, return the sum of the two children
   int tm = (tl + tr) / 2;
-  return sum(v*2, tl, tm, l, min(r, tm)) + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
+  return sum(v*2, tl, tm, l, min(r, tm)) 
+  + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
 }
 
 // Update the value of some position
@@ -46,6 +47,63 @@ void update(int v, int tl, int tr, int pos, int newVal) {
       update(v*2+1, tm+1, tr, pos, newVal);
     }
     // After the update, the current vertex need a update too
+    // based on its children values
     segtree[v] = segtree[v*2] + segtree[v*2+1];
   }
 }
+
+// Test Sum Segment Tree
+int main() {
+	int n;
+	cout << "Array's Length: ";
+	cin >> n;
+	
+	cout << "Elements:\n";
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	build(arr, 1, 0, n-1);
+	
+	while (true) {
+		cout << "\nChoose one option:\n";
+		cout << "1 - get sum between L and R, use: '1 L R'\n";
+		cout << "2 - set number X in pos I, use: '2 X I'\n";
+		cout << "3 - see initial array\n";
+		cout << "4 - exit\n";
+		cout << "> ";
+		
+		int option;
+		cin >> option;
+		
+		if (option == 4) break;
+		
+		else if (option == 3) {
+			for (int i = 0; i < n; i++) {
+				cout << arr[i] << " ";
+			}
+			cout << endl;
+		}
+		
+		else if (option == 2) {
+			int pos, newVal;
+			cin >> newVal >> pos;
+			
+			update(1, 0, n-1, pos, newVal);
+			
+			cout << "Value updated!\n";
+		}
+		
+		else if (option == 1) {
+			int l, r;
+			cin >> l >> r;
+			
+			int result = sum(1, 0, n-1, l, r);
+			
+			cout << "The sum of this segment is " << result << endl;
+		}
+	}
+
+	return 0;
+}
+
